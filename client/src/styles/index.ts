e91d2ddf59b763a295df86e9cbe35011bd04be47
Daniel Lazar <lazardani0112@gmail.com>
2021-09-02 21:49:03 +0200

    Added global card style.

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -4,6 +4,12 @@ const globStyles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
+	card: {
+		marginBottom: 15,
+		paddingHorizontal: 20,
+		paddingVertical: 15,
+		borderRadius: 10,
+	},
 });
 
 export default globStyles;
