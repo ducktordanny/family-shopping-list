a33418ad617933f47aa7600fd99156f00bb48e59
Daniel Lazar <lazardani0112@gmail.com>
2021-09-15 17:18:27 +0200

    Added logic to GroupScreen and UI other changes.

--- client/src/screens/GroupScreen.tsx
+++ client/src/screens/GroupScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import {
-	Text,
 	FlatList,
 	View,
 	SafeAreaView,
 	TouchableOpacity,
+	Alert,
 } from 'react-native';
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -17,10 +17,13 @@ import tw from 'tailwind-react-native-classnames';
 import HeaderView from '../containers/HeaderView';
 import { Label, SubTitle, Title } from '../components/Texts';
 import Icon from '../components/Icon';
-import { LabelButton } from '../components/Buttons';
+import { IconLabelButton, LabelButton } from '../components/Buttons';
 import globStyles from '../styles';
 import ProductCard from '../components/ProductCard';
 import GoBackIcon from '../components/GoBackIcon';
+import API, { getHeaders } from '../API';
+import axios from 'axios';
+import Clipboard from '@react-native-community/clipboard';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Group'>;
 type GroupScreenNavigationProp = Props['navigation'];
@@ -30,7 +33,10 @@ const GroupScreen = () => {
 	const { groupId } = useRoute<GroupScreenRouteProp>().params;
 	const navigation = useNavigation<GroupScreenNavigationProp>();
 	const { token } = useStoreState(state => state.user);
-	const [products, groupInfo, _addProduct] = useProducts(token, groupId);
+	const { products, groupInfo, setProducts, addProduct } = useProducts(
+		token,
+		groupId,
+	);
 
 	const navigateToMembers = () => {
 		if (groupInfo === null) return undefined;
@@ -40,21 +46,87 @@ const GroupScreen = () => {
 		});
 	};
 
+	const navigateToProduct = (id: string) => {
+		// TODO: we should include the followings: product: ProductProps, groupName: string
+		if (!groupInfo || !products) return undefined;
+		const product = products.find(element => element._id === id);
+		if (!product) return undefined;
+		navigation.navigate('Product', { groupName: groupInfo?.name, product });
+	};
+
+	const checkProduct = async (id: string, isChecked: boolean) => {
+		if (products === null) return undefined;
+		try {
+			const response = await axios.patch(
+				`${API}/products/${isChecked ? 'unbought' : 'bought'}/${id}`,
+				undefined,
+				getHeaders(token || ''),
+			);
+			const prodIndex = products.findIndex(element => element._id === id);
+			setProducts(current => {
+				if (current) {
+					return current.map((element, index) =>
+						index === prodIndex ? response.data : element,
+					);
+				}
+				return current;
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const addProductTrigger = () => {
+		Alert.prompt('Add new product:', undefined, [
+			{ text: 'Cancel', style: 'destructive' },
+			{
+				text: 'Add',
+				onPress: async content => {
+					try {
+						const newProduct = await axios.post(
+							`${API}/products/create`,
+							{
+								groupId,
+								content,
+							},
+							getHeaders(token || ''),
+						);
+						addProduct(newProduct.data);
+					} catch (err) {
+						console.log(err);
+					}
+				},
+			},
+		]);
+	};
+
+	const inviteTrigger = () => {
+		Clipboard.setString(groupId);
+		Alert.alert(
+			'Invite',
+			'The Id has copied to the clipboard. Other people can join with that Id.',
+		);
+	};
+
 	return (
 		<>
 			<HeaderView>
 				<View style={tw`flex-row justify-between items-center`}>
 					<GoBackIcon />
-					<Title style={tw`m-0`}>{groupInfo?.name}</Title>
+					<Title style={tw`m-0`}>{groupInfo?.name || '...'}</Title>
 					<TouchableOpacity onPress={() => console.log('Hello World')}>
 						<Icon icon="menu" />
 					</TouchableOpacity>
 				</View>
 				<View style={tw`items-center`}>
 					<SubTitle>Creator</SubTitle>
-					<Label>{groupInfo?.createdBy.name}</Label>
+					<Label>{groupInfo?.createdBy.name || '...'}</Label>
 					<SubTitle>Created at</SubTitle>
-					<Label>{new Date(groupInfo?.createdAt || '').toDateString()}</Label>
+					<Label>
+						{groupInfo?.createdAt
+							? new Date(groupInfo.createdAt).toDateString()
+							: '...'}
+					</Label>
 					<LabelButton
 						label="See members"
 						style={tw`pb-0 mb-0`}
@@ -70,12 +142,15 @@ const GroupScreen = () => {
 							data={products}
 							renderItem={({ item }) => (
 								<ProductCard
+									id={item._id}
 									content={item.content}
 									important={item.important}
 									addedBy={item.addedBy.name}
 									createdAt={item.createdAt}
 									boughtBy={item.boughtBy?.name || null}
-									boughtAt={item.boughtAt || null}
+									boughtAt={item.boughtAt}
+									onCheck={checkProduct}
+									onPress={navigateToProduct}
 								/>
 							)}
 							keyExtractor={item => item._id}
@@ -88,6 +163,20 @@ const GroupScreen = () => {
 				) : (
 					<Loading />
 				)}
+				<View style={tw`flex-row justify-center`}>
+					<IconLabelButton
+						label="Add product"
+						icon="plus"
+						style={{ marginVertical: 15 }}
+						onPress={addProductTrigger}
+					/>
+					<IconLabelButton
+						label="Invite"
+						icon="plus"
+						style={{ marginVertical: 15 }}
+						onPress={inviteTrigger}
+					/>
+				</View>
 			</SafeAreaView>
 		</>
 	);
