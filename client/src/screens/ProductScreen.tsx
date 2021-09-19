27555f95b3c14dcda3c40860dcef283d2b3f6bf4
Daniel Lazar <lazardani0112@gmail.com>
2021-09-19 14:45:22 +0200

    Fixes with KeyboardAvoidingView.

--- client/src/screens/ProductScreen.tsx
+++ client/src/screens/ProductScreen.tsx
@@ -1,25 +1,27 @@
-import React, { useEffect } from 'react';
-import { View, SafeAreaView, ScrollView } from 'react-native';
+import React from 'react';
+import {
+	View,
+	SafeAreaView,
+	ScrollView,
+	KeyboardAvoidingView,
+	Platform,
+} from 'react-native';
 import { useRoute } from '@react-navigation/core';
 import HeaderView from '../containers/HeaderView';
 import GoBackIcon from '../components/GoBackIcon';
 import { RootStackParamList } from '../types/NavigationProps';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import tw from 'tailwind-react-native-classnames';
-import globStyles from '../styles';
 import { Label, SubTitle, Title } from '../components/Texts';
 import MiniUserCard from '../components/MiniUserCard';
 import TextField from '../components/TextField';
+import globStyles from '../styles';
+import tw from 'tailwind-react-native-classnames';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Product'>;
 type ProductScreenRouteProp = Props['route'];
 
 const ProductScreen = () => {
 	const { groupName, product } = useRoute<ProductScreenRouteProp>().params;
-
-	useEffect(() => {
-		console.log('Product screen test', groupName, product);
-	}, [groupName, product]);
 
 	return (
 		<>
@@ -31,34 +33,45 @@ const ProductScreen = () => {
 				</View>
 				<View style={tw`items-center`}>
 					<SubTitle style={{ marginTop: 10 }}>{product.content}</SubTitle>
-					<SubTitle>Added</SubTitle>
-					<MiniUserCard
-						name={product.addedBy.name}
-						picture={product.addedBy.picture}
-					/>
-					<Label style={tw`m-0`}>
-						{new Date(product.createdAt || '').toDateString()}
-					</Label>
-					{product.boughtAt !== null && product.boughtBy && (
-						<>
-							<SubTitle>Bought</SubTitle>
-							<MiniUserCard
-								name={product.boughtBy.name}
-								picture={product.boughtBy.picture}
-							/>
-							<Label style={tw`m-0`}>
-								{new Date(product.boughtAt || '').toDateString()}
-							</Label>
-						</>
-					)}
 				</View>
 			</HeaderView>
-			<SafeAreaView style={[tw`justify-between`, globStyles.container]}>
-				<ScrollView style={globStyles.card}>
-					<SubTitle style={tw`m-0`}>Edit</SubTitle>
-					<TextField placeholder="Test" upperLabel="Test" />
-				</ScrollView>
-			</SafeAreaView>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+				style={{ flex: 1 }}>
+				<SafeAreaView style={tw`justify-between`}>
+					<ScrollView style={[globStyles.card, { marginBottom: 0 }]}>
+						<View style={tw`items-center`}>
+							<SubTitle style={tw`m-0`}>Added</SubTitle>
+							<MiniUserCard
+								name={product.addedBy.name}
+								picture={product.addedBy.picture}
+							/>
+							<Label style={tw`m-0`}>
+								{new Date(product.createdAt || '').toDateString()}
+							</Label>
+							{product.boughtAt !== null && product.boughtBy && (
+								<>
+									<SubTitle>Bought</SubTitle>
+									<MiniUserCard
+										name={product.boughtBy.name}
+										picture={product.boughtBy.picture}
+									/>
+									<Label style={tw`m-0`}>
+										{new Date(product.boughtAt || '').toDateString()}
+									</Label>
+								</>
+							)}
+						</View>
+						<SubTitle style={tw`m-0`}>Edit</SubTitle>
+						<Label>
+							Hello World this is something what doesn't mean anything...
+						</Label>
+						<TextField placeholder="Test" />
+						<TextField placeholder="Test" />
+						<View style={{ height: 60 }}></View>
+					</ScrollView>
+				</SafeAreaView>
+			</KeyboardAvoidingView>
 		</>
 	);
 };
