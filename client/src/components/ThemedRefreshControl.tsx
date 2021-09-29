a0de396b982eb3749fdf3cddb788c5cd1abe960f
Daniel Lazar <lazardani0112@gmail.com>
2021-09-29 17:35:08 +0200

    Added themed refresh control.

---
+++ client/src/components/ThemedRefreshControl.tsx
@@ -0,0 +1,10 @@
+import { useTheme } from '@react-navigation/native';
+import React from 'react';
+import { RefreshControl, RefreshControlProps } from 'react-native';
+
+const ThemedRefreshControl = (props: RefreshControlProps) => {
+	const theme = useTheme();
+	return <RefreshControl tintColor={theme.colors.text} {...props} />;
+};
+
+export default ThemedRefreshControl;
